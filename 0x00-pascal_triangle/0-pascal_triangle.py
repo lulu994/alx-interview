14c9ba9ab82ef37553c06d29ef457a8b19c0adc9
lulu994 <loubna.neddah@gmail.com>
2024-03-05 09:48:05 +0100

    pascal_triangle(n)

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""
+Returns a list of lists of integers representing the Pascal’s triangle of n
+"""
 
 
 def pascal_triangle(n: int):
+    """Returns an empty list if n <= 0"""
 
     if n <= 0:
         return []
