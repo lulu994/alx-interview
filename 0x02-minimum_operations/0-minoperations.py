39caa17ab5c4eaacd2028c9f4b634e786fd031a5
lulu994 <loubna.neddah@gmail.com>
2024-03-19 11:17:26 +0100

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,26 +7,29 @@ def minOperations(n):
     '''Computes the fewest number of operations needed to result
     in exactly n H characters.
     '''
-
-    if not isinstance(n, int) or n <= 0:
+    if not isinstance(n, int):
         return 0
-    
-    operations = 0
-    divisor = 2
-    
-    while n > 1:
-        if n % divisor == 0:
-            n //= divisor
-            operations += divisor
-        else:
-            divisor += 1
-    
-    return operations
-
-# Example usage:
-n = 4
-print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
-
-n = 12
-print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
-
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
